7f9eb7879bdd9f11eac949c02c04b845814dda30
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-06 20:48:47 +0100

    autoload preview at startup correctly

--- static/controls/preview/preview.js
+++ static/controls/preview/preview.js
@@ -1,41 +1,121 @@
 define(['c'], function(c) {
+  var PreviewOp = {
+    STRAIGHT_PROBE:      1,
+    RIGID_TAP:           2,
+    STRAIGHT_FEED:       3,
+    ARC_FEED:            4,
+    STRAIGHT_TRAVERSE:   5,
+    SET_G5X_OFFSET:      6,
+    SET_G92_OFFSET:      7,
+    SET_XY_ROTATION:     8,
+    SELECT_PLANE:        9,
+    SET_TRAVERSE_RATE:   10,
+    SET_FEED_RATE:       11,
+    CHANGE_TOOL:         12,
+    CHANGE_TOOL_NUMBER:  13,
+    DWELL:               14,
+    MESSAGE:             15,
+    COMMENT:             16,
+    USE_TOOL_OFFSET:     17,
+    SET_PARAMS:          18, // kins, axismask, angle_units, length_unit,
+    SET_FEED_MODE:       19,
+    SOURCE_CONTEXT:      20,
+  };
+
   return function createPreview(machine) {
+    var currentFile = null;
+    var color = new THREE.Color(0xffffff);
+    var travelColor = new THREE.Color(0x333333);
+
     var previewObject = new THREE.Object3D();
     var material = new THREE.LineBasicMaterial({
-      color: 0xff0000,
+      color: 0xffffff,
       opacity: 0.8,
-      linewidth: 2
+      linewidth: 1,
+      transparent: true,
+      vertexColors: THREE.VertexColors
     });
     machine.on('preview', function(preview) {
-      var geometry = createGeometry(preview);
-      var line = new THREE.Line(geometry, material);
-      previewObject.add(line);
+      createObjects(preview);
+      endPath();
     });
 
-    function createGeometry(preview) {
-      var positions = new Float32Array(preview.length * 3);
-      var j = 0;
+    machine.on('status', function(status) {
+      if (status.task.file !== currentFile) {
+        currentFile = status.task.file;
+        machine.command('emcTaskPlanOpen', ['preview', currentFile]);
+        machine.command('emcTaskPlanRun', ['preview', 0]);
+      }
+    });
+
+    function createObjects(preview) {
+
       for(var i=0;i<preview.length;i++) {
         var item = preview[i];
         switch(item.type) {
-          case 3:
-          case 5:
+          case PreviewOp.ARC_FEED:
+            processPathArc(item);
+            break;
+          case PreviewOp.STRAIGHT_FEED:
+          case PreviewOp.STRAIGHT_TRAVERSE:
             // HACK: This converts inches to mm.
             // TODO: Figure out where units for previews are exposed.
             preview[i].pos.x *= 25.4;
             preview[i].pos.y *= 25.4;
             preview[i].pos.z *= 25.4;
-            positions[j * 3 + 0] = preview[i].pos.x;
-            positions[j * 3 + 1] = preview[i].pos.y;
-            positions[j * 3 + 2] = preview[i].pos.z;
-            j++;
+
+            processPathStraight(item);
             break;
         }
       }
-      var geometry = new THREE.BufferGeometry();
-      geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-      geometry.setDrawRange(0, j);
-      return geometry;
+    }
+    var currentPathItem = null;
+
+    function processPathStraight(item) {
+      if (currentPathItem && currentPathItem.type !== 'line') {
+        endPath();
+      }
+      if (!currentPathItem) {
+        currentPathItem = {
+          type: 'line',
+          geometry: new THREE.Geometry()
+        };
+      }
+
+      currentPathItem.geometry.vertices.push(new THREE.Vector3(
+        item.pos.x,
+        item.pos.y,
+        item.pos.z
+      ));
+      currentPathItem.geometry.colors.push(item.type == 3
+        ? color
+        : travelColor
+      );
+    }
+
+    function processPathArc(item) {
+      if (currentPathItem && currentPathItem.type !== 'arc') {
+        endPath();
+      }
+      if (!currentPathItem) {
+        currentPathItem = {
+          type: 'arc',
+          geometry: new THREE.Geometry()
+        };
+      }
+      console.log(item);
+    }
+
+    function endPath() {
+      if (currentPathItem === null) { return; }
+      switch(currentPathItem.type) {
+        case 'line':
+          previewObject.add(new THREE.Line(currentPathItem.geometry, material));
+          break;
+        case 'arc':
+          break;
+      }
+      currentPathItem = null;
     }
 
     return previewObject;
