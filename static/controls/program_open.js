8e8510361b4843198051f0a29f9fbd776bd990bb
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-06 21:30:39 +0100

    when opening programs, use remote_path if path is not rooted

--- static/controls/program_open.js
+++ static/controls/program_open.js
@@ -3,6 +3,12 @@ define(['c'], function(c) {
     var pathinput;
 
     function onopen() {
+      var path = pathinput.value;
+      if (path[0] !== '/') {
+        // Add remote_path in front of path.
+        // for instance $HOME/machinekit/nc_files
+        path = machine.status.config.remote_path + '/' + path;
+      }
       machine.command('emcTaskSetMode', ['execute', 2]);
       machine.command('emcTaskPlanOpen', ['execute', pathinput.value]);
     }
