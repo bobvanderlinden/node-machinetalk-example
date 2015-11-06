b8571153af3ca7117668466655abeafd032ac36e
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-06 17:24:04 +0100

    fix: convert inches to mm for preview

--- static/controls/preview/preview.js
+++ static/controls/preview/preview.js
@@ -16,11 +16,21 @@ define(['c'], function(c) {
       var positions = new Float32Array(preview.length * 3);
       var j = 0;
       for(var i=0;i<preview.length;i++) {
-        if (preview[i].type !== 3)  { continue; }
-        positions[j * 3 + 0] = preview[i].pos.x;
-        positions[j * 3 + 1] = preview[i].pos.y;
-        positions[j * 3 + 2] = preview[i].pos.z;
-        j++;
+        var item = preview[i];
+        switch(item.type) {
+          case 3:
+          case 5:
+            // HACK: This converts inches to mm.
+            // TODO: Figure out where units for previews are exposed.
+            preview[i].pos.x *= 25.4;
+            preview[i].pos.y *= 25.4;
+            preview[i].pos.z *= 25.4;
+            positions[j * 3 + 0] = preview[i].pos.x;
+            positions[j * 3 + 1] = preview[i].pos.y;
+            positions[j * 3 + 2] = preview[i].pos.z;
+            j++;
+            break;
+        }
       }
       var geometry = new THREE.BufferGeometry();
       geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
