b10474b76894f0671e058eaf161471cfb6ebbb1b
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-06 20:49:44 +0100

    added history to MDI

--- static/controls/mdi.js
+++ static/controls/mdi.js
@@ -2,16 +2,37 @@ define(['eventbus','c'], function(eventbus, c) {
   return function createMdiControl(machine) {
     var mdiInput = c('input', {placeholder: ''}, []);
     var executeButton = c('button', {class: 'ui button'}, 'Execute');
+    var history = c.div({ class: 'history', style: 'overflow: auto; height: 20em;'}, []);
 
-    executeButton.onclick = function() {
-      machine.command('emcTaskSetMode', ['execute', 3]);
-      machine.command('emcTaskPlanExecute', ['execute', mdiInput.value]);
-      mdiInput.value = '';
+    executeButton.onclick = execute;
+    mdiInput.onkeypress = function(e) {
+      if (e.keyCode === 13 || e.keyCode === 10) {
+        e.preventDefault();
+        execute();
+      }
     };
+    function execute() {
+      var command = mdiInput.value;
+      machine.command('emcTaskSetMode', ['execute', 3]);
+      machine.command('emcTaskPlanExecute', ['execute', command]);
 
-    return c('div', {class: 'ui action input'}, [
-      mdiInput,
-      executeButton
+      var historyItem = c.div({}, command);
+      historyItem.onclick = fromHistory.bind(null, command);
+      history.appendChild(historyItem);
+      history.scrollTop = history.scrollHeight;
+
+      mdiInput.value = '';
+    }
+    function fromHistory(command) {
+      mdiInput.value = command;
+    }
+
+    return c.div({ }, [
+      history,
+      c('div', {class: 'ui action input'}, [
+        mdiInput,
+        executeButton
+      ])
     ]);
   };
 });
